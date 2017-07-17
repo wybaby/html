bb8c8e37a46e99339ad2dbb5f2bd66558d667e25
wybaby <wybaby@gmail.com>
2017-07-17 10:10:29 +0800

    增加24点

---
+++ 24.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <link rel='icon' href='/ico/24.ico'>
+        <link rel='apple-touch-icon' href='/ico/24.jpg'>
+        <title>24点</title>
+    </head>
+    <body>
+    <?php
+    $Tab4 = "    ";
+    $seed = 200000;
+    srand(rand(1,$seed));
+    ?>
+    <table border='1' align='center'>
+        <tr>
+            <td width=20 align='center'><?php srand(rand(1,$seed)); echo rand(1,13);?></td>
+            <td width=20 align='center'><?php srand(rand(1,$seed)); echo rand(1,13);?></td>
+            <td width=20 align='center'><?php srand(rand(1,$seed)); echo rand(1,13);?></td>
+            <td width=20 align='center'><?php srand(rand(1,$seed)); echo rand(1,13);?></td>
+        <tr>
+    </table>
+    </body>
+</html>
